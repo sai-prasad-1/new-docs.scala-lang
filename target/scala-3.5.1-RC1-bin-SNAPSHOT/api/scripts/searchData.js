538dab8338b2435c62128ab208854508f1adab2a
sai-prasad-1 <sai.prasad.1603@gmail.com>
2024-07-05 20:36:57 +0530

    Add API folder to git

---
+++ target/scala-3.5.1-RC1-bin-SNAPSHOT/api/scripts/searchData.js
@@ -0,0 +1,10 @@
+pages = [{"l":"index.html#","e":false,"i":"","n":"ScalaDocSiteBuilder","t":"ScalaDocSiteBuilder","d":"","k":"static","x":""},
+{"l":"$lessempty$greater$/Main$.html#","e":false,"i":"","n":"Main","t":"Main","d":"","k":"object","x":""},
+{"l":"$lessempty$greater$/Main$.html#main-913","e":false,"i":"","n":"main","t":"main(args: Array[String]): Unit","d":"Main","k":"def","x":""},
+{"l":"$lessempty$greater$/StringUtils$.html#","e":false,"i":"","n":"StringUtils","t":"StringUtils","d":"","k":"object","x":""},
+{"l":"$lessempty$greater$/StringUtils$.html#isPalindrome-fffffd1b","e":false,"i":"","n":"isPalindrome","t":"isPalindrome(s: String): Boolean","d":"StringUtils","k":"def","x":""},
+{"l":"$lessempty$greater$/StringUtils$.html#reverse-fffff2ca","e":false,"i":"","n":"reverse","t":"reverse(s: String): String","d":"StringUtils","k":"def","x":""},
+{"l":"$lessempty$greater$/StringUtils$.html#toUpperCase-fffff2ca","e":false,"i":"","n":"toUpperCase","t":"toUpperCase(s: String): String","d":"StringUtils","k":"def","x":""},
+{"l":"docs/index.html#","e":false,"i":"","n":"ScalaDocSiteBuilder","t":"ScalaDocSiteBuilder","d":"","k":"static","x":""},
+{"l":"docs/getting-started.html#","e":false,"i":"","n":"Getting Started","t":"Getting Started","d":"","k":"static","x":""},
+{"l":"docs/guide.html#","e":false,"i":"","n":"Guide","t":"Guide","d":"","k":"static","x":""}];
